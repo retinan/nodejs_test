2171a106ae201290748464450398592cb17e2d56
retin <voicemin78@gmail.com>
2020-05-17 18:56:09 +0900

    admin base template update

--- routes/admin.js
+++ routes/admin.js
@@ -2,9 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('admin/products.html', 
-        { message : 'hello nodejs'}
-    );
+    res.render('admin/products.html');
 });
 
 module.exports = router;
